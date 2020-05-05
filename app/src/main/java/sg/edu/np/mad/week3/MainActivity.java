25fd61fe811ffeabdac56f5acc9bc01e4d827b45
Yee Jun Eugene Long <s10193060@connect.np.edu.sg>
2020-05-05 16:02:56 +0800

    update files

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,33 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer media;
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+
+        video = (VideoView) findViewById(R.id.videoView);
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        video.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        media.release();
+        media = null;
     }
 }
